c8718bab1ff50a303e22ca455155d33d0ec9955d
paulina.kuzmicka@gmail.com <paulina.kuzmicka@gmail.com>
2017-10-03 20:30:01 +0200

    wpis w gitignore, usuniecie out/ z gita, modyfikacja klasy Calculator

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -9,4 +9,11 @@ public class Calculator {
     public long substractAFromB(int a, int b){
         return b - a;
     }
+
+    public static void main (String[] args) {
+
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.addAToB(5, 4));
+        System.out.println(calculator.substractAFromB(8,2));
+    }
 }
