e2bd5a09aa709da2153c735f7550ad691e4f6b98
paulina.kuzmicka@gmail.com <paulina.kuzmicka@gmail.com>
2017-10-03 21:53:05 +0200

    poprawiona klasa

--- src/main/java/com/kodilla/CalculatorApplication.java
+++ src/main/java/com/kodilla/CalculatorApplication.java
@@ -9,8 +9,6 @@ public class CalculatorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
 
-		Calculator calculator = new Calculator();
-		System.out.println(calculator.addAToB(5, 4));
-		System.out.println(calculator.substractAFromB(8,2));
+
 	}
 }
