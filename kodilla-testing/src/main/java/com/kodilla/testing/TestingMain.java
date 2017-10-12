6c248b42aae1f03d6b32c9d2d97ba72c0209cc9f
paulina.kuzmicka@gmail.com <paulina.kuzmicka@gmail.com>
2017-10-12 12:47:36 +0200

    Dodanie klasy SimpleUser i Calculator oraz przetestowanie metod

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,9 +1,37 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania.");
 
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUsername();
+
+        if(result.equals("theForumUser")){
+            System.out.println("test OK");
+        }else{
+            System.out.println("Error!");
+        }
+
+        //Calculator
+
+        Calculator calculator = new Calculator();
+        long calculatorAdd = calculator.addAToB(5, 3);
+
+        if(calculatorAdd == 8){
+            System.out.println("test OK");
+        }else{
+            System.out.println("Error!");
+        }
+
+        long calculatorSubstract = calculator.substractAFromB(3, 20);
+
+        if(calculatorSubstract == 17){
+            System.out.println("test OK");
+        }else{
+            System.out.println("Error!");
+        }
     }
-
 }
