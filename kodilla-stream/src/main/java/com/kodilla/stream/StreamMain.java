118b2fbf0a33911289eda038c23ec8f84f63b6d6
paulina.kuzmicka@gmail.com <paulina.kuzmicka@gmail.com>
2017-10-22 22:41:24 +0200

    Nowy modul - stream; upiekszacz tekstow

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,4 +1,44 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.*;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
 public class StreamMain {
+    public static void main(String[]args) {
+        System.out.println("Welcome to module 7 - Stream");
+        Processor processor = new Processor();
+        //Executor codeToExecute = () -> System.out.println("This is an example text.");
+        //processor.execute(codeToExecute);
+        processor.execute(() -> System.out.println("This is an example text."));
+
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        String beautifulText = poemBeautifier.beautify("Text to beautify", (text -> text.toUpperCase()));
+        System.out.println(beautifulText);
+
+        String beautifulText2 = poemBeautifier.beautify("Text to beautify", (text -> "ABC" + text + "ABC"));
+        System.out.println(beautifulText2);
+
+        String beautifulText3 = poemBeautifier.beautify("Text to beautify", (text -> text.replace(" ", "*")));
+        System.out.println(beautifulText3);
+
+        String beautifulText4 = poemBeautifier.beautify("Text to beautify", (text -> text.length() + " letters ***" + text + "*** " + text.length() + " letters"));
+        System.out.println(beautifulText4);
+
+        String beautifulText5 = poemBeautifier.beautify("Text to beautify", (text -> text.concat(String.valueOf(text.hashCode()))));
+        System.out.println(beautifulText5);
+    }
 }
--- kodilla-stream/src/main/java/com/kodilla/stream/lambda/Executor.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/lambda/Executor.java
@@ -1,4 +1,5 @@
 package com.kodilla.stream.lambda;
 
 public interface Executor {
+    void process();
 }
