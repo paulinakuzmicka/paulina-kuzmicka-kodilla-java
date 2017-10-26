7d77ae171d2b1e088d074e40086f5e3b0e3e02bb
paulina.kuzmicka@gmail.com <paulina.kuzmicka@gmail.com>
2017-10-26 11:34:38 +0200

    Counting average using IntStream

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,17 @@
+package com.kodilla.stream.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ArrayOperationsTestSuite {
+    @Test
+    public void testGetAverage(){
+        //Given
+        int numbers [] = {1,9,4,6,3,7,5,5,10,0,3,7,5,5,9,1,2,8,5,5};
+        double averageExpected = 5.0;
+        //When
+        double average = ArrayOperations.getAverage(numbers);
+        //Then
+        Assert.assertEquals(averageExpected, average, 0);
+    }
+}
