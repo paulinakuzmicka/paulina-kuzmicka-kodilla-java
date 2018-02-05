962a7b9300afa6ec9167bf97a489d967b5a6af6f
paulina.kuzmicka@gmail.com <paulina.kuzmicka@gmail.com>
2018-02-05 21:26:28 +0100

    testUpdateBestsellers corrections

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -35,7 +35,7 @@ public class StoredProcTestSuite {
     public void testUpdateBestsellers()throws SQLException{
         //Given
         DbManager dbManager = DbManager.getInstance();
-        String sqlUpdate = "UPDATE books SET bestseller=0";
+        String sqlUpdate = "UPDATE books SET bestseller=null";
         Statement statement = dbManager.getConnection().createStatement();
         statement.executeUpdate(sqlUpdate);
 
@@ -44,12 +44,12 @@ public class StoredProcTestSuite {
         statement.execute(sqlProcedureCall);
 
         //Then
-        String sqlCheckTable="SELECT COUNT(*) AS how_many FROM books WHERE bestseller=0";
+        String sqlCheckTable="SELECT COUNT(*) AS how_many FROM books WHERE bestseller=null";
         ResultSet rs = statement.executeQuery(sqlCheckTable);
         int howMany=-1;
         if(rs.next()){
             howMany=rs.getInt("how_many");
         }
-        assertEquals(1,howMany);
+        assertEquals(0,howMany);
     }
 }
